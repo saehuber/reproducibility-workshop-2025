591112756da36303487f50e0d535fd563c1a13d5
shuber <sarah.huber@westdri.ca>
2024-05-30 13:20:32 -0700

    added edited take-home files for automation exercise

---
+++ instructor-files/automation_exercise/data/download_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# create the dataset folder
+mkdir -p dataset
+
+# enter the dataset folder
+cd dataset
+
+# download the data
+wget https://object-arbutus.cloud.computecanada.ca/RCSWorkshopMedia/2024-06-10-COSS-Reproducible-Research/astronauts_full.csv
+wget https://object-arbutus.cloud.computecanada.ca/RCSWorkshopMedia/2024-06-10-COSS-Reproducible-Research/readme.md
+
+# change the name of the dataset
+mv astronauts_full.csv astronauts.csv
+
+# move back to original directory
+cd ..
