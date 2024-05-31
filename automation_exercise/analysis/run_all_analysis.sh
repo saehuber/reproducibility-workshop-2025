8466fd5f14c139e4d224d277b7fa439a6d956a05
shuber <sarah.huber@westdri.ca>
2024-05-30 22:05:29 -0700

    add print-out about python computational environment to analysis script

--- automation_exercise/analysis/run_all_analysis.sh
+++ automation_exercise/analysis/run_all_analysis.sh
@@ -2,14 +2,13 @@
 module load python/3.11.5
 module load scipy-stack/2023b
 
+#print information about python computational environment to file
+python --version > computational_env.txt
+pip freeze >> computational_env.txt
+
+#run all python files
 for file in *.py
 do
   echo "Running $file"
   python "$file"
 done
-
-
-#idea: pass paths as variables to the scripts
-#if name=main in python scripts??
-
-#python astronaut_spacewalk_record_age_plot.py
