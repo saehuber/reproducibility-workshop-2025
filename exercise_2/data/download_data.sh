08c7f632cf57b924ac6e05adc7ffe4882ed27bc1
Sarah Huber <saehuber@users.noreply.github.com>
2025-06-04 15:03:40 -0700

    Update exercise 2

--- exercise_2/data/download_data.sh
+++ exercise_2/data/download_data.sh
@@ -7,8 +7,8 @@ mkdir -p dataset
 cd dataset
 
 # download the data
-wget https://object-arbutus.cloud.computecanada.ca/RCSWorkshopMedia/2024-06-10-COSS-Reproducible-Research/astronauts_1980.csv
-wget https://object-arbutus.cloud.computecanada.ca/RCSWorkshopMedia/2024-06-10-COSS-Reproducible-Research/readme.md
+wget https://object-arbutus.cloud.computecanada.ca/RCSWorkshopMedia/2025-06-10-COSS-Reproducible-Research/astronauts_1980.csv
+wget https://object-arbutus.cloud.computecanada.ca/RCSWorkshopMedia/2025-06-10-COSS-Reproducible-Research/readme.md
 
 # change the name of the dataset
 mv astronauts_1980.csv astronauts.csv
