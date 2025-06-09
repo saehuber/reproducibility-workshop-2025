2fa956f309cf93621c5f2197d97f15d0c59372ae
shuber <sarah.huber@westdri.ca>
2025-06-09 13:19:13 -0700

    typos

--- instructor-files/exercise_2/demo/loops/python_1.py
+++ instructor-files/exercise_2/demo/loops/python_1.py
@@ -1,6 +1,5 @@
 import os
 for item in os.listdir(os.getcwd()):
-   item_path = os.path.join(os.getcwd(),item)
+    item_path = os.path.join(os.getcwd(),item)
     if os.path.isdir(item_path):
         print(item_path)
-
--- instructor-files/exercise_2/demo/loops/python_2.py
+++ instructor-files/exercise_2/demo/loops/python_2.py
@@ -1,6 +1,6 @@
 import os
 current_dir = os.getcwd()
 for item in os.listdir(current_dir):
-   item_path = os.path.join(current_dir, item)
+    item_path = os.path.join(current_dir, item)
     if os.path.isdir(item_path):
         print(item_path)
